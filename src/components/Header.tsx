e5104ee75a7a79cf3cd319cc7837ab2e01b32e27
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-06-19 18:34:14 -0300

    style: add responsiveness to header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,7 +4,7 @@ import logo from '../assets/seidor-logo.png'
 
 export default function Header() {
   return (
-    <header className="fixed left-0 top-0 flex h-20 w-full items-center justify-between bg-slate-600 p-4 shadow">
+    <header className="fixed left-0 top-0 flex h-20 w-full items-center justify-between bg-slate-600 p-4 shadow sm:px-8 md:px-16 lg:px-32">
       <div className="p-2">
         <img className="w-24" src={logo} alt="logo da empresa saidor" />
       </div>
