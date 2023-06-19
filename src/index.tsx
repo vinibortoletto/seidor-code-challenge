10567d7963e457275ba7e066ead12a0f6dc435df
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-06-19 17:36:38 -0300

    chore: remove boilerplate files

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
+import App from 'App'
 import { createRoot } from 'react-dom/client'
 import 'tailwindcss/tailwind.css'
-import App from 'components/App'
 
 const container = document.getElementById('root') as HTMLDivElement
 const root = createRoot(container)
