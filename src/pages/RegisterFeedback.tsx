58f87d495ede7ae9747cbf85c5de48f83cb2b75f
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-06-20 00:32:03 -0300

    refactor: register feedback title

--- src/pages/RegisterFeedback.tsx
+++ src/pages/RegisterFeedback.tsx
@@ -4,7 +4,7 @@ import Title from '../components/Title'
 export default function RegisterFeedback() {
   return (
     <>
-      <Title text="Cadastro de feedbacks" />
+      <Title text="Cadastrar feedback" />
       <Form />
     </>
   )
