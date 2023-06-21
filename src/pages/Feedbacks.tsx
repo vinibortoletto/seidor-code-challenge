1ebaccfa9b4dc1d615f67c108cb4d060a931f662
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-06-21 17:15:24 -0300

    feat: display message if there are no feedbacks

--- src/pages/Feedbacks.tsx
+++ src/pages/Feedbacks.tsx
@@ -5,18 +5,23 @@ import Filters from '../components/Filters'
 import { FeedbackContext } from '../contexts/FeedbackContext'
 
 export default function Feedbacks() {
-  const { filteredFeedbacks } = useContext(FeedbackContext)
+  const { feedbacks, filteredFeedbacks } = useContext(FeedbackContext)
 
   return (
     <>
       <Title text="Feedbacks" />
-
       <Filters />
 
       <div className="flex flex-col gap-4">
-        {filteredFeedbacks.map((feedback) => (
-          <FeedbackCard key={feedback.id} feedback={feedback} />
-        ))}
+        {feedbacks.length === 0 ? (
+          <p className="text-center text-2xl text-slate-500">
+            Nenhum feedback cadastrado
+          </p>
+        ) : (
+          filteredFeedbacks.map((feedback) => (
+            <FeedbackCard key={feedback.id} feedback={feedback} />
+          ))
+        )}
       </div>
     </>
   )
